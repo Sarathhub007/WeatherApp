96ed06353160a9bb36f80414e6bce373cfe093b1
Sarathhub007 <ilamsarathchandra@gmail.com>
2025-05-11 03:42:50 +0530

    updated

--- src/Components/Forgot-password.jsx
+++ src/Components/Forgot-password.jsx
@@ -1,6 +1,6 @@
 import  { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Forgot-Password.css"; 
+import "./Forgot-password.css"; 
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
